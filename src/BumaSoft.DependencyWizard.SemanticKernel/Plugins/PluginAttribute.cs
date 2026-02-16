630a4b26b8b969f75a1fa0b803259b2f3f5d4840
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-02-16 11:05:06 -0300

    Creating PluginAttribute

---
+++ src/BumaSoft.DependencyWizard.SemanticKernel/Plugins/PluginAttribute.cs
@@ -0,0 +1,9 @@
+using BumaSoft.DependencyWizard.Services;
+
+namespace BumaSoft.DependencyWizard.SemanticKernel.Plugins;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+public class PluginAttribute : Attribute
+{
+    public ServiceScope Scope { get; set; } = ServiceScope.Scoped;
+}
