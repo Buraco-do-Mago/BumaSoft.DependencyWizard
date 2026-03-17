508667500c46cb288eb6d463af4583fbba38c1e5
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-03-17 19:24:53 -0300

    Changing how the ServiceInjector works, so it considers the new properties of the attribute

--- src/BumaSoft.DependencyWizard/Services/ServiceInjector.cs
+++ src/BumaSoft.DependencyWizard/Services/ServiceInjector.cs
@@ -18,13 +18,38 @@ public static class ServiceInjector
             .GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static)
             .FirstOrDefault(method => method.IsGenericMethodDefinition && method.Name.Contains(nameof(InjectService)))!;
 
+        var injectServiceAsAbstractionMethod = typeof(ServiceInjector)
+            .GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static)
+            .FirstOrDefault(method => method.IsGenericMethodDefinition && method.Name.Contains(nameof(InjectServiceAsAbstraction)))!;
+
+        var errors = new List<string>();
+
         foreach (var serviceType in serviceTypes)
         {
             var serviceAttribute = serviceType.GetCustomAttribute<ServiceAttribute>();
             var scope = serviceAttribute!.Scope;
-            var injectServiceGenericMethod = injectServiceMethod.MakeGenericMethod(serviceType);
-            services = (IServiceCollection)injectServiceGenericMethod!.Invoke(null, [scope, services])!;
+
+            if (serviceAttribute.InjectionType is not null && serviceAttribute.InjectionMode is InjectionMode.Concrete)
+                errors.Add($"Service {serviceType.FullName} cannot be injected as concrete type because it has a specified injection type.");
+
+            if (serviceAttribute.InjectionType is null && serviceAttribute.InjectionMode is InjectionMode.Abstraction or InjectionMode.Both)
+                errors.Add($"Service {serviceType.FullName} cannot be injected as abstraction because it does not have a specified injection type.");
+
+            if (serviceAttribute.InjectionMode is InjectionMode.Concrete or InjectionMode.Both)
+            {
+                var injectServiceGenericMethod = injectServiceMethod.MakeGenericMethod(serviceType);
+                services = (IServiceCollection)injectServiceGenericMethod!.Invoke(null, [scope, services])!;
+            }
+
+            if (serviceAttribute.InjectionMode is InjectionMode.Abstraction or InjectionMode.Both)
+            {
+                var injectServiceAsAbstractionGenericMethod = injectServiceAsAbstractionMethod.MakeGenericMethod(serviceAttribute.InjectionType!, serviceType);
+                services = (IServiceCollection)injectServiceAsAbstractionGenericMethod!.Invoke(null, [scope, services])!;
+            }
         }
+
+        if (errors.Count != 0)
+            throw new InvalidOperationException(string.Join(Environment.NewLine, errors));
 
         return services;
     }
@@ -41,6 +66,27 @@ public static class ServiceInjector
                 break;
             case ServiceScope.Transient:
                 services.AddTransient<TService>();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(scope), scope, null);
+        }
+        return services;
+    }
+
+    private static IServiceCollection InjectServiceAsAbstraction<TInjectedService, TService>(ServiceScope scope, IServiceCollection services)
+        where TInjectedService : class
+        where TService : class, TInjectedService
+    {
+        switch (scope)
+        {
+            case ServiceScope.Singleton:
+                services.AddSingleton<TInjectedService, TService>();
+                break;
+            case ServiceScope.Scoped:
+                services.AddScoped<TInjectedService, TService>();
+                break;
+            case ServiceScope.Transient:
+                services.AddTransient<TInjectedService, TService>();
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(scope), scope, null);
